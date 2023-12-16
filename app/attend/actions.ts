f0d7d7637484a898f391928f9b7d46878dfb6806
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-15 17:22:22 -0800

    Verified actions.ts

--- app/attend/actions.ts
+++ app/attend/actions.ts
@@ -1,16 +1,19 @@
 "use server";
 
+import { v } from "@/utils";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 
 export async function updateExcuse(code: string, user: string, status: number) {
 	const cookieStore = cookies();
 	const client = createClient(cookieStore);
-	const res = await client
-		.from("attendance")
-		.update({ status })
-		.eq("code_used", code)
-		.eq("student", user)
-		.select();
+	const res = v(
+		await client
+			.from("attendance")
+			.update({ status })
+			.eq("code_used", code)
+			.eq("student", user)
+			.select(),
+	);
 	return res;
 }
