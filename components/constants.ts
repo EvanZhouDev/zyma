c4017bff3628bceb96f7f1793b17d33998c7c978
Evan Zhou <evanzhoudev@gmail.com>
2023-12-16 19:57:23 -0800

    ♻️ Refactoring :)

--- components/constants.ts
+++ components/constants.ts
@@ -1,11 +1,20 @@
 export const ATTENDENCE_STATUS_PRESENT = 0;
 export const STATUS_MAP = { 0: "Present" };
 export function convertStatus(status: number) {
-	if (status === 0) return "Present";
-	if (status === 1) return "Doctor's Appointment";
-	if (status === 2) return "Family Issues";
-	if (status === 3) return "Busy With Other Clubs";
-	if (status === 4) return "Homework";
-	if (status === 5) return "Other";
-	return "Absent";
+	switch (status) {
+		case 0:
+			return "Present";
+		case 1:
+			return "Doctor's Appointment";
+		case 2:
+			return "Family Issues";
+		case 3:
+			return "Busy With Other Clubs";
+		case 4:
+			return "Homework";
+		case 5:
+			return "Other";
+		default:
+			return "Absent";
+	}
 }
