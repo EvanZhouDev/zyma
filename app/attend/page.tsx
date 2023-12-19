c15cf7dd3bbbac34a629694dd42ee82c1bff93cf
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-19 11:15:14 -0800

    :recycle: Refactored /attend

--- app/attend/page.tsx
+++ app/attend/page.tsx
@@ -5,14 +5,37 @@ import Excuse from "./components/Excuse";
 
 export default async function Page({
 	searchParams,
-}: { searchParams: { code: string } }) {
+}: { searchParams: { code?: string } }) {
 	const cookieStore = cookies();
 	const client = createClient(cookieStore);
 	const studentId = (await client.auth.getUser()).data?.user?.id;
 	if (studentId == null) {
 		return redirect("/");
 	}
-
+	if (searchParams.code === undefined) {
+		return (
+			<div className="hero min-h-screen bg-neutral">
+				<div className="hero-content text-center">
+					<div role="alert" className="alert alert-error">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="stroke-current shrink-0 h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						<span>No code provided</span>
+					</div>
+				</div>
+			</div>
+		);
+	}
 	const { data, error } = await client
 		.from("attendance")
 		.upsert({
@@ -21,30 +44,6 @@ export default async function Page({
 		})
 		.select("status");
 	if (error != null || data === null) {
-		if (error.code === "23502") {
-			return (
-				<div className="hero min-h-screen bg-neutral">
-					<div className="hero-content text-center">
-						<div role="alert" className="alert alert-error">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								className="stroke-current shrink-0 h-6 w-6"
-								fill="none"
-								viewBox="0 0 24 24"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-								/>
-							</svg>
-							<span>No code provided</span>
-						</div>
-					</div>
-				</div>
-			);
-		}
 		console.assert(error.code === "23503", JSON.stringify(error));
 		return (
 			<div className="hero min-h-screen bg-neutral">
