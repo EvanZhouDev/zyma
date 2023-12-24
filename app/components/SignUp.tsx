aacac2dcaa015c7d4f37bf976773212fd966ffdf
Evan Zhou <evanzhoudev@gmail.com>
2023-12-24 11:57:57 -0800

    🐛 Fixed modal not in center of screen

--- app/components/SignUp.tsx
+++ app/components/SignUp.tsx
@@ -11,40 +11,45 @@ export default function SignUp({
 }) {
 	const nameDialog = useRef<HTMLDialogElement>(null);
 	return (
-		<form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-			<div className="w-full">
-				<label className="label">
-					<span className="text-base label-text">Email</span>
-				</label>
-				<input
-					required
-					type="email"
-					name="email"
-					className="w-full input input-standard form-input"
-				/>
-			</div>
-			<div className="w-full">
-				<label className="label">
-					<span className="text-base label-text">Password</span>
-				</label>
-				<input
-					required
-					type="password"
-					name="password"
-					className="w-full input input-standard form-input"
-				/>
-			</div>
-			<button className="btn btn-standard !rounded-lg mt-5" formAction={signIn}>
-				Sign In
-			</button>
-			<button
-				className="underline opacity-50 text-lg mt-10"
-				formAction={async () => {
-					nameDialog.current!.showModal();
-				}}
-			>
-				No account? Sign Up
-			</button>
+		<>
+			<form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+				<div className="w-full">
+					<label className="label">
+						<span className="text-base label-text">Email</span>
+					</label>
+					<input
+						required
+						type="email"
+						name="email"
+						className="w-full input input-standard form-input"
+					/>
+				</div>
+				<div className="w-full">
+					<label className="label">
+						<span className="text-base label-text">Password</span>
+					</label>
+					<input
+						required
+						type="password"
+						name="password"
+						className="w-full input input-standard form-input"
+					/>
+				</div>
+				<button
+					className="btn btn-standard !rounded-lg mt-5"
+					formAction={signIn}
+				>
+					Sign In
+				</button>
+				<button
+					className="underline opacity-50 text-lg mt-10"
+					formAction={async () => {
+						nameDialog.current!.showModal();
+					}}
+				>
+					No account? Sign Up
+				</button>
+			</form>
 			<dialog ref={nameDialog} className="modal">
 				<div className="modal-box">
 					<h3 className="font-bold text-lg">Hello!</h3>
@@ -71,6 +76,6 @@ export default function SignUp({
 					</button>
 				</div>
 			</dialog>
-		</form>
+		</>
 	);
 }
