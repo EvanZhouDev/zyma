cd5fc75576aeb57480dbe7fd31ea48a478e1557b
Evan Zhou <evanzhoudev@gmail.com>
2023-12-25 13:03:11 -0800

    ✨ Added new sign up modal

--- app/components/SignUp.tsx
+++ app/components/SignUp.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function SignUp({
 	signUp,
@@ -10,6 +10,8 @@ export default function SignUp({
 	signIn: (formData: FormData) => Promise<void>;
 }) {
 	const nameDialog = useRef<HTMLDialogElement>(null);
+	const [role, setRole] = useState("host");
+
 	return (
 		<>
 			<form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
@@ -51,30 +53,87 @@ export default function SignUp({
 				</button>
 			</form>
 			<dialog ref={nameDialog} className="modal">
-				<div className="modal-box">
-					<h3 className="font-bold text-lg">Hello!</h3>
-					<p className="py-4">Enter your name to continue.</p>
-					<div>
-						<label className="label">
-							<span className="text-base label-text">Name</span>
-						</label>
-						<input
-							type="text"
-							name="name"
-							className="w-full input input-standard mb-10"
-						/>
+				<form
+					onSubmit={async (event) => {
+						event.preventDefault();
+						const formData = new FormData(event.currentTarget);
+						console.log(formData.get("role"));
+						// await signUp(formData);
+						// nameDialog.current!.close();
+					}}
+				>
+					<div className="modal-box">
+						<h3 className="font-bold text-lg">Zyma Sign Up</h3>
+						<p className="py-4">
+							We need a little bit of information before we can start taking
+							attendance.
+						</p>
+						<div>
+							<label className="label">
+								<span className="text-base label-text">Name</span>
+							</label>
+							<input
+								type="text"
+								name="name"
+								className="w-full input input-standard mb-5"
+							/>
+						</div>
+						<div>
+							<label className="label">
+								<span className="text-base label-text">Email</span>
+							</label>
+							<input
+								type="email"
+								name="email"
+								className="w-full input input-standard mb-5"
+							/>
+						</div>
+						<div>
+							<label className="label">
+								<span className="text-base label-text">Password</span>
+							</label>
+							<input
+								type="password"
+								name="password"
+								className="w-full input input-standard mb-5"
+							/>
+						</div>
+						<div className="h-min">
+							<label className="label">
+								<span className="text-base label-text">Who are You?</span>
+							</label>
+							<select
+								className="select input-standard w-full"
+								name="role"
+								value={role}
+								onChange={(e) => setRole(e.target.value)}
+							>
+								<option value="host">Host</option>
+								<option value="attendee">Attendee</option>
+							</select>
+
+							{role === "host" && (
+								<div className="ml-1 mt-2 mb-10">
+									Hosts cannot join Groups or Attend.
+									<br />
+									<b>You will not be able to change your role later.</b>
+								</div>
+							)}
+
+							{role === "attendee" && (
+								<div className="ml-1 mt-2 mb-10">
+									Attendees cannot host Sessions.
+									<br />
+									<b>You will not be able to change your role later.</b>
+								</div>
+							)}
+						</div>
+
+						<button type="submit" className="btn btn-standard">
+							Submit
+						</button>
 					</div>
-					<button
-						type="submit"
-						className="btn btn-standard"
-						formAction={async (formData: FormData) => {
-							await signUp(formData);
-							nameDialog.current!.close();
-						}}
-					>
-						Submit
-					</button>
-				</div>
+				</form>
 			</dialog>
 		</>
 	);
