8b253a0c4b350a8a8cd3d3744c762d13e97c585d
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-27 13:25:11 -0800

    :art: Format and lint

--- app/actions.ts
+++ app/actions.ts
@@ -6,51 +6,51 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function signIn(
-  redirectTo: string | undefined,
-  formData: FormData
+	redirectTo: string | undefined,
+	formData: FormData,
 ) {
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
-  const client = getServerClient();
+	const email = formData.get("email") as string;
+	const password = formData.get("password") as string;
+	const client = getServerClient();
 
-  const { error } = await client.auth.signInWithPassword({
-    email,
-    password,
-  });
+	const { error } = await client.auth.signInWithPassword({
+		email,
+		password,
+	});
 
-  if (error) {
-    const redirectURL = new URL(headers().get("origin")!);
-    redirectURL.searchParams.append("error", error.message);
-    if (redirectTo) {
-      redirectURL.searchParams.append("redirectTo", redirectTo);
-    }
+	if (error) {
+		const redirectURL = new URL(headers().get("origin")!);
+		redirectURL.searchParams.append("error", error.message);
+		if (redirectTo) {
+			redirectURL.searchParams.append("redirectTo", redirectTo);
+		}
 
-    return redirect(redirectURL.href);
-  }
-  return redirect(redirectTo ?? "/host/dashboard");
+		return redirect(redirectURL.href);
+	}
+	return redirect(redirectTo ?? "/host/dashboard");
 }
 export async function signUp(formData: FormData) {
-  const origin = headers().get("origin");
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
-  const name = formData.get("name") as string;
+	const origin = headers().get("origin");
+	const email = formData.get("email") as string;
+	const password = formData.get("password") as string;
+	const name = formData.get("name") as string;
 
-  const client = getServerClient();
+	const client = getServerClient();
 
-  const { error } = await client.auth.signUp({
-    email,
-    password,
-    options: {
-      data: {
-        name: name,
-      },
-      emailRedirectTo: `${origin}/host/dashboard`,
-    },
-  });
+	const { error } = await client.auth.signUp({
+		email,
+		password,
+		options: {
+			data: {
+				name: name,
+			},
+			emailRedirectTo: `${origin}/host/dashboard`,
+		},
+	});
 
-  if (error) {
-    return redirect(`/?message=${error}`);
-  }
+	if (error) {
+		return redirect(`/?message=${error}`);
+	}
 
-  return redirect("/?message=Check email to continue sign in process");
+	return redirect("/?message=Check email to continue sign in process");
 }
