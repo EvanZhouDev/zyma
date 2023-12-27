46be1931b05a38c457ffdea82d9c38a3eabd4d97
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-27 09:31:19 -0800

    :art: Lint and formatted

--- global-setup.ts
+++ global-setup.ts
@@ -1,21 +1,21 @@
-import type { FullConfig } from "@playwright/test";
 import { exec } from "child_process";
+import type { FullConfig } from "@playwright/test";
 
-export default async function globalSetup(config: FullConfig) {
-  await new Promise<void>((resolve, reject) => {
-    const childProcess = exec("supabase db reset", (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error: ${error.message}`);
-        reject(error);
-      } else {
-        console.log(`stdout: ${stdout}`);
-        resolve();
-      }
-    });
+export default async function globalSetup(_: FullConfig) {
+	await new Promise<void>((resolve, reject) => {
+		const childProcess = exec("supabase db reset", (error, stdout, _) => {
+			if (error) {
+				console.error(`Error: ${error.message}`);
+				reject(error);
+			} else {
+				console.log(`stdout: ${stdout}`);
+				resolve();
+			}
+		});
 
-    childProcess.on("error", (error) => {
-      console.error(`Error: ${error.message}`);
-      reject(error);
-    });
-  });
+		childProcess.on("error", (error) => {
+			console.error(`Error: ${error.message}`);
+			reject(error);
+		});
+	});
 }
