48ca79c2855ec272c7a26fde97c970145327a62d
ThatXliner <bryan.hu.2020@gmail.com>
2023-12-30 10:39:56 -0800

    Refactored attendee info

--- app/host/dashboard/components/AttendeeActions.tsx
+++ app/host/dashboard/components/AttendeeActions.tsx
@@ -2,36 +2,12 @@ import { InfoIcon, TrashIcon } from "@primer/octicons-react";
 import { useRef } from "react";
 import { removeAttendee } from "../actions";
 import { Attendee } from "../contexts";
+import AttendeeInfo from "./AttendeeInfo";
 
 export default function AttendeeActions({ attendee }: { attendee: Attendee }) {
-	const modal = useRef<HTMLDialogElement>(null);
 	return (
 		<div className="flex">
-			<button
-				onClick={() => modal.current!.showModal()}
-				className="btn btn-standard ml-2"
-			>
-				<InfoIcon size="medium" />
-			</button>
-			<dialog ref={modal} className="modal">
-				<div className="modal-box">
-					<h3 className="font-bold text-lg">
-						Attendee Name: {attendee.username}
-					</h3>
-					<p className="py-4 mt-5">
-						The Email associated with this group is {attendee.email}.
-					</p>
-					<p className="py-4 font-normal">
-						Attendance:{" "}
-						{JSON.stringify(attendee?.metadata?.attendanceHistory ?? {})}
-					</p>
-					<div className="modal-action">
-						<form method="dialog">
-							<button className="btn btn-standard">Close</button>
-						</form>
-					</div>
-				</div>
-			</dialog>
+			<AttendeeInfo attendee={attendee} />
 			<button
 				className="btn btn-dangerous ml-2 transition-none"
 				onClick={async () => {
