fe4bccdb71f64b88b79a61dce742b8dff4bb207f
Evan Zhou <evanzhoudev@gmail.com>
2023-12-29 21:38:01 -0800

    ✨ Added "Your Last Attendance"

--- app/host/dashboard/components/Dashboard.tsx
+++ app/host/dashboard/components/Dashboard.tsx
@@ -16,7 +16,7 @@ import AttendeeTable from "./AttendeeTable";
 import GroupTable from "./GroupTable";
 import NewGroup from "./NewGroup";
 import RegisterAttendee from "./RegisterAttendee";
-import ExportButton from "./ExportButton"
+import ExportButton from "./ExportButton";
 
 async function getAttendee(uuid: string) {
 	const client = await createClient();
@@ -235,7 +235,7 @@ export default function Dashboard({
 												placeholder="Search Attendees..."
 												className="input input-standard mr-2 flex-grow"
 											/>
-											<ExportButton/>
+											<ExportButton />
 											<RegisterAttendee groupId={groupId} />
 										</div>
 										<div>
@@ -274,109 +274,147 @@ export default function Dashboard({
 					</div>
 				</div>
 			</div>
-			<div className="bg-base-100 rounded-box outline-base-200 m-3 ml-1.5 flex basis-2/5 flex-col items-center outline outline-1 justify-between">
-				<div className="flex flex-col items-center w-full">
-					<a
-						className={`btn-start-attendance p-3 m-2 h-[10vh] w-[90%] flex items-center justify-center text-2xl font-semibold ${
-							groups[0] === undefined ? "btn-disabled btn-start-disabled" : ""
-						} mt-5 flex items-center justify-center`}
-						href={`/host/attendance?groupId=${groupId}`}
-					>
-						<RepoIcon
-							size="small"
-							verticalAlign="middle"
-							className="mr-2 w-10 h-10"
-						/>
-						<div className="flex flex-col">
-							Start Attendance
-							<span className="font-normal opacity-75 text-lg">
-								for {attendees.length} registered attendees
-							</span>
+			<div className="m-3 ml-1.5 flex basis-2/5 flex-col items-center justify-between">
+				<div className="bg-base-100 rounded-box outline-base-200 flex flex-col items-center outline outline-1 justify-between w-full flex-grow">
+					<div className="flex flex-col items-center w-full">
+						<a
+							className={`btn-start-attendance p-3 m-2 h-[10vh] w-[90%] flex items-center justify-center text-2xl font-semibold ${
+								groups[0] === undefined ? "btn-disabled btn-start-disabled" : ""
+							} mt-5 flex items-center justify-center`}
+							href={`/host/attendance?groupId=${groupId}`}
+						>
+							<RepoIcon
+								size="small"
+								verticalAlign="middle"
+								className="mr-2 w-10 h-10"
+							/>
+							<div className="flex flex-col">
+								Start Attendance
+								<span className="font-normal opacity-75 text-lg">
+									for {attendees.length} registered attendees
+								</span>
+							</div>
+						</a>
+						{groups[0] === undefined ? (
+							<p className="opacity-50 text-center">
+								Cannot start attendance session without a group.
+							</p>
+						) : attendees.length === 0 ? (
+							<p className="opacity-50 text-center max-w-lg">
+								Zyma will not be able to track attendance without registered
+								attendees.
+							</p>
+						) : undefined}
+						{/* TODO: Refactor into config component */}
+						<div className="w-full px-5 !mt-5">
+							<h1 className="website-title !text-secondary-content !text-2xl">
+								Configure Your Attendance Session:
+							</h1>
+							<label className="form-control my-2 w-full">
+								<div className="label">
+									<span className="label-text">
+										Session Length (Leave empty for indefinite)
+									</span>
+								</div>
+								<input
+									type="text"
+									placeholder="Length in seconds..."
+									className="input input-standard w-full"
+								/>
+							</label>
+							<label className="form-control my-2 w-full">
+								<div className="label">
+									<span className="label-text">
+										Session Timeout (Leave empty for indefinite)
+									</span>
+								</div>
+								<input
+									type="text"
+									placeholder="Timeout in seconds..."
+									className="input input-standard w-full"
+								/>
+							</label>
+							<label className="form-control my-2 flex w-full max-w-xs flex-row items-center">
+								<div className="form-control">
+									<label className="label cursor-pointer">
+										<input
+											type="checkbox"
+											defaultChecked={true}
+											className="checkbox checkbox-primary [--chkfg:white] rounded-md"
+										/>
+									</label>
+								</div>
+								<div className="label">
+									<span className="label-text">
+										Allow People Not in Group
+										<br />
+									</span>
+								</div>
+							</label>
 						</div>
-					</a>
-					{groups[0] === undefined ? (
-						<p className="opacity-50 text-center">
-							Cannot start attendance session without a group.
-						</p>
-					) : attendees.length === 0 ? (
-						<p className="opacity-50 text-center max-w-lg">
-							Zyma will not be able to track attendance without registered
-							attendees.
-						</p>
-					) : undefined}
-					{/* TODO: Refactor into config component */}
-					<div className="w-full px-5 !mt-5">
-						<h1 className="website-title !text-secondary-content !text-2xl">
-							Configure Your Attendance Session:
-						</h1>
-						<label className="form-control my-2 w-full">
-							<div className="label">
-								<span className="label-text">
-									Session Length (Leave empty for indefinite)
-								</span>
-							</div>
-							<input
-								type="text"
-								placeholder="Length in seconds..."
-								className="input input-standard w-full"
-							/>
-						</label>
-						<label className="form-control my-2 w-full">
-							<div className="label">
-								<span className="label-text">
-									Session Timeout (Leave empty for indefinite)
-								</span>
-							</div>
-							<input
-								type="text"
-								placeholder="Timeout in seconds..."
-								className="input input-standard w-full"
-							/>
-						</label>
-						<label className="form-control my-2 flex w-full max-w-xs flex-row items-center">
-							<div className="form-control">
-								<label className="label cursor-pointer">
-									<input
-										type="checkbox"
-										defaultChecked={true}
-										className="checkbox checkbox-primary [--chkfg:white] rounded-md"
-									/>
-								</label>
-							</div>
-							<div className="label">
-								<span className="label-text">
-									Allow People Not in Group
-									<br />
-								</span>
-							</div>
-						</label>
 					</div>
 				</div>
-				<div className="flex flex-row items-center justify-between w-full mb-5 px-5">
-					<Logo className="ml-2" size={125} />
-					<div className="flex flex-row items-center justify-between">
-						<SwitchTheme />
+				<div className="bg-base-100 rounded-box outline-base-200 flex flex-col items-center outline outline-1 justify-between w-full mt-3">
+					<div className="flex flex-col items-center justify-between w-full mb-5 px-5">
+						<div className="flex justify-between items-center w-full mt-3">
+							<div className="flex flex-col">
+								<h1 className="font-bold !text-secondary-content !text-2xl">
+									Your Last Attendance:
+								</h1>
+								<p className="text-secondary-content text-xl opacity-50">
+									On 12/29/23 for 36.4k attendees
+								</p>
+							</div>
+							<button className="btn btn-standard">See Full Statistics</button>
+						</div>
+						<div className="stats w-full overflow-hidden mt-5 mb-3">
+							<div className="stat">
+								<div className="stat-title">Attendees joined</div>
+								<div className="stat-value">31K</div>
+								<div className="stat-desc">85% of total</div>
+							</div>
 
-						<a
-							className="mx-5"
-							href="https://github.com/EvanZhouDev/zyma/issues"
-							title="Report an issue"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<ReportIcon className="w-8 h-8" />
-						</a>
-						<button
-							className="btn btn-dangerous"
-							onClick={async () => {
-								const supabase = await createClient();
-								await supabase.auth.signOut();
-								window.location.reload();
-							}}
-						>
-							<SignOutIcon className="w-8 h-8" />
-							Log Out
-						</button>
+							<div className="stat">
+								<div className="stat-title">Attendees Absent</div>
+								<div className="stat-value">4,200</div>
+								<div className="stat-desc">12% of total</div>
+							</div>
+
+							<div className="stat">
+								<div className="stat-title">Unregistered</div>
+								<div className="stat-value">1,200</div>
+								<div className="stat-desc">3% of total</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="bg-base-100 rounded-box outline-base-200 flex flex-col items-center outline outline-1 justify-between w-full mt-3">
+					<div className="flex flex-row items-center justify-between w-full mb-5 mt-5 px-5">
+						<Logo className="ml-2" size={125} />
+						<div className="flex flex-row items-center justify-between">
+							<SwitchTheme />
+
+							<a
+								className="mx-5"
+								href="https://github.com/EvanZhouDev/zyma/issues"
+								title="Report an issue"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<ReportIcon className="w-8 h-8" />
+							</a>
+							<button
+								className="btn btn-dangerous"
+								onClick={async () => {
+									const supabase = await createClient();
+									await supabase.auth.signOut();
+									window.location.reload();
+								}}
+							>
+								<SignOutIcon className="w-8 h-8" />
+								Log Out
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
