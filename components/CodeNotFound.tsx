383cb3215f0ea2d1e757cc86ff650eddf48fac93
Evan Zhou <evanzhoudev@gmail.com>
2023-12-31 20:48:25 -0800

    🥅 Some better errors for users

--- components/CodeNotFound.tsx
+++ components/CodeNotFound.tsx
@@ -3,11 +3,27 @@ import ErrorInfo from "./ErrorInfo";
 export default function CodeNotFound({
 	action,
 	code,
-}: { action: string; code: string }) {
+}: {
+	action: string;
+	code: string;
+}) {
 	return (
 		<ErrorInfo
 			title={`Could not ${action} this group.`}
-			footer="The Attendance Session may have ended. If you are sure it hasn't, please try again, ensuring you entered the code correctly."
+			footer={
+				<div>
+					<b>The Attendance Session may have ended.</b>
+					<br />
+					<br />
+					<p className="opacity-50">
+						If you are sure it hasn't, please try again, ensuring you entered
+						the code correctly.{" "}
+						<a href="/attendee" className="underline">
+							Return Home
+						</a>
+					</p>
+				</div>
+			}
 		>
 			<span>
 				Code <b>{code}</b> not found.
