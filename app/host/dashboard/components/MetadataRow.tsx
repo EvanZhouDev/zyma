2a1a9478fc4ed2db5ebf2b70e8b748c803c22f35
ThatXliner <bryan.hu.2020@gmail.com>
2024-01-01 11:38:32 -0800

    Add useDebounce hook and update MetadataRow component

--- app/host/dashboard/components/MetadataRow.tsx
+++ app/host/dashboard/components/MetadataRow.tsx
@@ -1,5 +1,6 @@
 import { TrashIcon } from "@primer/octicons-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDebounce } from "usehooks-ts";
 
 export default function MetadataRow({
 	row,
@@ -13,6 +14,14 @@ export default function MetadataRow({
 	deleteRow: (key: string) => Promise<void>;
 }) {
 	const [value, setValue] = useState<string>(originalValue);
+	const debouncedValue = useDebounce<string>(value, 500);
+	useEffect(() => {
+		// Do fetch here...
+		// Triggers when "debouncedValue" changes
+		(async () => {
+			await editRow(row, debouncedValue);
+		})();
+	}, [debouncedValue]);
 	return (
 		<tr>
 			<td>{row}</td>
@@ -23,9 +32,6 @@ export default function MetadataRow({
 					onChange={(event) => {
 						setValue(event.target.value);
 					}}
-					onInput={async () => {
-						await editRow(row, value);
-					}}
 					value={value}
 					className="input input-standard w-fit max-w-xs"
 				/>
@@ -34,7 +40,8 @@ export default function MetadataRow({
 				<div className="flex space-x-2">
 					<button
 						className="btn btn-dangerous transition-none"
-						onClick={async () => {
+						onClick={async (event) => {
+							event.preventDefault();
 							await deleteRow(row);
 						}}
 					>
