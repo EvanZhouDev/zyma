e1de6ff7ce4b913754ccfc8c26fa09afb8d72f13
Evan Zhou <evanzhoudev@gmail.com>
2024-01-01 21:33:01 -0800

    ✨ Working on making Your Last Attendance work

--- app/host/dashboard/components/YourLastAttendance.jsx
+++ app/host/dashboard/components/YourLastAttendance.jsx
@@ -1,7 +1,57 @@
-import { useRef } from "react";
+"use client";
+import { useRef, useContext } from "react";
+import { AttendeesInClassContext } from "../contexts";
 
 export default function YourLastAttendance() {
 	const lastAttendanceDialog = useRef(null);
+	const attendeesHistory = useContext(AttendeesInClassContext).map(
+		(x) => x.metadata.attendanceHistory,
+	);
+	console.log(attendeesHistory.lengthHistory);
+
+	if (attendeesHistory.length === 0) {
+		return (
+			<div className="flex flex-col items-center justify-between w-full mb-5 px-5 pt-5">
+				<h1 className="font-bold !text-secondary-content !text-2xl">
+					Your Last Attendance
+				</h1>
+				<p className="text-secondary-content text-xl opacity-50">
+					Take attendance to view statistics
+				</p>
+			</div>
+		);
+	}
+
+	const now = new Date();
+
+	let allDates = [];
+
+	for (const attendee of attendeesHistory) {
+		allDates = allDates.concat(Object.keys(attendee));
+	}
+
+	console.log(allDates);
+
+	const dateObjects = allDates.map((date) => new Date(date));
+
+	// Find the closest date
+	const closestDate = `${dateObjects
+		.reduce((a, b) => {
+			return Math.abs(b - now) < Math.abs(a - now) ? b : a;
+		})
+		.toISOString()
+		.slice(0, -5)}Z`;
+
+	const attendees = attendeesHistory
+		.map((x) => {
+			if (x[closestDate] === undefined) return undefined;
+			return x[closestDate][1];
+		})
+		.filter((x) => x !== undefined);
+
+	const attendeesPresent = attendees.filter((x) => x === 0).length;
+	const attendeesAbsent = attendees.filter((x) => x !== 0).length;
+	const attendeesTotal = attendees.length;
 
 	return (
 		<div className="flex flex-col items-center justify-between w-full mb-5 px-5">
@@ -11,7 +61,7 @@ export default function YourLastAttendance() {
 						Your Last Attendance:
 					</h1>
 					<p className="text-secondary-content text-xl opacity-50">
-						On 12/29/23 for 36.4k attendees
+						On 12/29/23 for {attendeesTotal} attendees
 					</p>
 				</div>
 				<button
@@ -29,28 +79,34 @@ export default function YourLastAttendance() {
 							<div className="flex flex-col justify-center">
 								<h1 className="text-2xl font-bold">Your Last Attendance:</h1>
 								<p className="text-xl opacity-50">
-									On 12/29/23 for 36.4k attendees
+									On 12/29/23 for {attendees.length} attendees
 								</p>
 							</div>
 
 							<div className="stats w-[70%] overflow-hidden ml-5">
 								<div className="stat">
-									<div className="stat-title">Attendees joined</div>
-									<div className="stat-value">31K</div>
-									<div className="stat-desc">85% of total</div>
+									<div className="stat-title">Attendees Present</div>
+									<div className="stat-value">{attendeesPresent}</div>
+									<div className="stat-desc">
+										{Math.round((attendeesPresent / attendeesTotal) * 100)}% of
+										total
+									</div>
 								</div>
 
 								<div className="stat">
 									<div className="stat-title">Attendees Absent</div>
-									<div className="stat-value">4,200</div>
-									<div className="stat-desc">12% of total</div>
+									<div className="stat-value">{attendeesAbsent}</div>
+									<div className="stat-desc">
+										{Math.round((attendeesAbsent / attendeesTotal) * 100)}% of
+										total
+									</div>
 								</div>
-
+								{/* 
 								<div className="stat">
 									<div className="stat-title">Unregistered</div>
 									<div className="stat-value">1,200</div>
 									<div className="stat-desc">3% of total</div>
-								</div>
+								</div> */}
 							</div>
 						</div>
 						<div className="flex flex-row items-center w-full justify-between mt-4">
@@ -105,21 +161,19 @@ export default function YourLastAttendance() {
 			</div>
 			<div className="stats w-full overflow-hidden mt-5 mb-3">
 				<div className="stat">
-					<div className="stat-title">Attendees joined</div>
-					<div className="stat-value">31K</div>
-					<div className="stat-desc">85% of total</div>
+					<div className="stat-title">Attendees Present</div>
+					<div className="stat-value">{attendeesPresent}</div>
+					<div className="stat-desc">
+						{Math.round((attendeesPresent / attendeesTotal) * 100)}% of total
+					</div>
 				</div>
 
 				<div className="stat">
 					<div className="stat-title">Attendees Absent</div>
-					<div className="stat-value">4,200</div>
-					<div className="stat-desc">12% of total</div>
-				</div>
-
-				<div className="stat">
-					<div className="stat-title">Unregistered</div>
-					<div className="stat-value">1,200</div>
-					<div className="stat-desc">3% of total</div>
+					<div className="stat-value">{attendeesAbsent}</div>
+					<div className="stat-desc">
+						{Math.round((attendeesAbsent / attendeesTotal) * 100)}% of total
+					</div>
 				</div>
 			</div>
 		</div>
