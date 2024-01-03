44606bdcc1e45d00e0bae28032c16bd725ef6330
ThatXliner <bryan.hu.2020@gmail.com>
2024-01-03 13:55:04 -0800

    Added notice on how config doesn't work

--- app/host/dashboard/components/Dashboard.tsx
+++ app/host/dashboard/components/Dashboard.tsx
@@ -320,6 +320,25 @@ export default function Dashboard({
 							<h1 className="website-title !text-secondary-content !text-2xl">
 								Configure Your Attendance Session:
 							</h1>
+							<div role="alert" className="alert alert-warning">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="stroke-current shrink-0 h-6 w-6"
+									fill="none"
+									viewBox="0 0 24 24"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+									/>
+								</svg>
+								<span>
+									Currently, configuring your attendance session is not
+									implemented
+								</span>
+							</div>
 							<label className="form-control my-2 w-full">
 								<div className="label">
 									<span className="label-text">
